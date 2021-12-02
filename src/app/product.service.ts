5103ce1024e571bc379f6f0d731c46b2d258da73
Anita Ajah <anitaisbeta@gmail.com>
2021-12-02 14:13:51 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { Response} from '@angular/http';
+import { Http } from '@angular/http'
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
