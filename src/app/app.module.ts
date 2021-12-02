6d72fb05f49ed9b20ebbd92b293673e4d74975da
Anita Ajah <anitaisbeta@gmail.com>
2021-12-02 13:30:05 +0100

    importing product service to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-
+import { ProductService } from './product.service';
 @NgModule({
   declarations: [
     AppComponent,
