3b42ab2b53f0e6251466a0069136a331cbc883b2
Anita Ajah <anitaisbeta@gmail.com>
2021-12-03 07:39:45 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Response} from '@angular/http';
-import { Http } from '@angular/http'
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  
 
-
-  constructor() { }
-  getAlbum(id:number)
+  constructor(private _http: Http) { }
+  getAlbum(id:number): Observable<Album>{
+    return this_http.get(this._albumUrl).map(response => <album>response.json());
+  }
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productUrl).map(response => <Product[]>response.jason());
+  }
 
 }
